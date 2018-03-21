4891ca9fa70700980523ef6bb2c1f159f4373c20
xalanq <xalanq@gmail.com>
2018-03-22 07:21:31 +0800

    Add settings menu, complete about fragment, change splash picture

--- app/src/main/java/com/xalanq/coursehelper/AboutFragment.java
+++ app/src/main/java/com/xalanq/coursehelper/AboutFragment.java
@@ -8,9 +8,11 @@ package com.xalanq.coursehelper;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * 显示作者信息的碎片
@@ -20,6 +22,11 @@ public class AboutFragment extends BasicFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.about_fragment, container, false);
+        TextView author = view.findViewById(R.id.about_author);
+        author.setText(R.string.about_author);
+        TextView github = view.findViewById(R.id.about_github);
+        github.setText(R.string.about_github);
+        github.setMovementMethod(LinkMovementMethod.getInstance());
         return view;
     }
 }
